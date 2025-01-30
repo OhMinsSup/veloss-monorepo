80b9d3cbbc253edae73532e61631e5a806a0fad7
OhMinSeop <mins5190@gmail.com>
2025-01-30 16:49:25 +0900

    add: openapi-ofetch 문서 추가

--- packages/openapi-ofetch/src/index.ts
+++ packages/openapi-ofetch/src/index.ts
@@ -1,5 +1,11 @@
 export { createOpenApiFetch } from "./fetch";
-export { isFetchError, createFetchError, FetchError } from "./error";
+export {
+  isFetchError,
+  createFetchError,
+  FetchError,
+  type FetchErrorConstructorOptions,
+  type IFetchError,
+} from "./error";
 
 export * from "./global.types";
 export * from "./fetch.types";
